505c01b7bc105b1a6588be6cda15de17af4564f9
gabriel santos <gabrielhs.santos01@gmail.com>
2023-12-03 12:48:24 -0300

    ajuste no tipo do campo

--- Api.Books.Core/Entities/Author.cs
+++ Api.Books.Core/Entities/Author.cs
@@ -2,9 +2,10 @@
 {
     public class Author : Common
     {
-        public string? Name { get; set; }
+        public required string Name { get; set; }
         public string? Nickname { get; set; }
         public DateTime DateOfBirth { get; set; }
         public ICollection<Book>? Books { get; set; }
+        public required string DocumentNumber { get; set; }
     }
 }
